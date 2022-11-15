6f6bd7b09945dd63b3139964d68e8b765186d0b2
Daniel Livingston <daniel.livingston@amd.com>
2022-11-15 15:08:46 -0500

    Working demo

--- realtime-audio-rs/src/main.rs
+++ realtime-audio-rs/src/main.rs
@@ -1,20 +1,48 @@
-use cpal::platform::available_hosts;
 use cpal::traits::{HostTrait, DeviceTrait};
+use cpal::platform::HostId;
 
-fn main() {
-    println!("Hello, world!");
-
-    for host in available_hosts() {
-        println!("Found host: {:#?}", host.name());
+fn system_overview() {
+    let hosts: Vec<HostId> = cpal::platform::available_hosts();
+    println!("Available hosts (count = {}):", hosts.len());
+    for host in hosts {
+        println!("- {}", host.name());
     }
 
+    let default_host = cpal::default_host();
+    println!("\nUsing default host: {}", default_host.id().name());
+
+    let input_devices: Vec<cpal::Device> = default_host.input_devices().unwrap().collect();
+    println!("\nAvailable input devices (count = {}):", input_devices.len());
+    for device in input_devices {
+        println!("- {}", device.name().unwrap_or(String::from("ERROR_UNKNOWN")));
+    }
+
+    let output_devices: Vec<cpal::Device> = default_host.output_devices().unwrap().collect();
+    println!("\nAvailable output devices (count = {}):", output_devices.len());
+    for device in output_devices {
+        println!("- {}", device.name().unwrap_or(String::from("ERROR_UNKNOWN")));
+    }
+}
+
+fn microphone_sample_demo() {
     let host = cpal::default_host();
 
-    let names: Vec<String> = host.input_devices().unwrap().map(|x| x.name().unwrap()).collect();
-    println!("Found devices: {:#?}", names);
+    let output_device = host.default_output_device()
+                            .expect("no output device available");
 
-    let device = host.default_output_device()
-                     .expect("no output device available");
+    let input_device = host.default_input_device()
+                            .expect("no input device available");
 
-    println!("Initialized device '{:#?}'", device.name());
+    println!("Demo configuration:");
+    println!("  - Host:   '{}'", host.id().name());
+    println!("  - Input:  '{}'", input_device.name().unwrap());
+    println!("  - Output: '{}'", output_device.name().unwrap());
+}
+
+fn main() {
+    println!("\n\nDEVICE & DRIVER OVERVIEW\n");
+    system_overview();
+
+    println!("\n\nMICROPHONE SAMPLE DEMO\n");
+    microphone_sample_demo();
 }
