55a1fe6dc295ebe04507707f33ba690379d4b6dd
Daniel Livingston <daniel.livingston@live.com>
2022-11-17 21:45:30 -0500

    Working psarc header parsing!

--- afx-song-file-toolkit/src/main.rs
+++ afx-song-file-toolkit/src/main.rs
@@ -1,18 +1,32 @@
 use clap::Parser;
+use binrw::BinRead;
+
+use std::fs::File;
+use std::io::BufReader;
 
 #[derive(Parser)]
 struct Cli {
     path: std::path::PathBuf,
 }
 
+#[derive(BinRead, Debug)]
+struct Version(u16, u16);
+
 // https://www.psdevwiki.com/ps3/PlayStation_archive_(PSARC)
 // https://github.com/GaticusHax/libPSARC/blob/development/libPSARC-Static/Source/PSARC/Header.cs
 // https://docs.rs/binrw/latest/binrw/
 // https://jam1.re/blog/binread-a-declarative-rust-binary-parsing-library
 #[derive(BinRead, Debug)]
-#[br(little, magic = b"PSARC")]
+#[br(big, magic = b"PSAR")]
 struct PsarcHeader {
-
+    version: Version,
+    #[br(count = 4)]
+    compression: Vec<u8>,
+    toc_length: u32,
+    toc_entry_size: u32,
+    toc_entries: u32,
+    block_size: u32,
+    archive_flags: u32
 }
 
 struct PsarcFile {
@@ -37,30 +51,18 @@ fn as_u32_le(array: &[u8; 4]) -> u32 {
 
 fn parse_psarc(path: &std::path::PathBuf) -> PsarcFile {
     let filename = String::from(path.to_string_lossy());
-    let buffer = std::fs::read(&path).unwrap();
-
-    let identifier = std::str::from_utf8(&buffer[0..4]).unwrap();
-    let version = as_u32_be(&buffer[4..8].try_into().unwrap());
-    let compression = std::str::from_utf8(&buffer[8..12]).unwrap();
-
-    let toc_size = as_u32_be(&buffer[12..16].try_into().unwrap());
-    let toc_entry_size = as_u32_be(&buffer[16..20].try_into().unwrap());;
-    let entry_count = as_u32_be(&buffer[20..24].try_into().unwrap());;
-    let block_size = as_u32_be(&buffer[24..28].try_into().unwrap());;
-    let archive_flags = as_u32_be(&buffer[28..32].try_into().unwrap());;
-
     println!("Filename: {:?}", filename);
-    println!("Identifier: {:?}", identifier);
-    println!("Version: {}", version);
-    println!("Compression: {:?}", compression);
-    println!("TOC Size: {}", toc_size);
-    println!("TOC Entry Size: {}", toc_entry_size);
-    println!("Entry Count: {}", entry_count);
-    println!("Block Size: {}", block_size);
-    println!("Archive Flags: {}", archive_flags);
 
-    // Next up: implement primary contents reading; decryption; flags
-    // https://github.com/kokolihapihvi/Rocksmith2014PsarcLib/blob/06a078a872b7d9d8c0b7d0c58ca76543fdc095d4/Psarc/PsarcTOC.cs#L8
+    let f = File::open(&path).unwrap();
+    let mut reader = BufReader::new(f);
+
+    let header = PsarcHeader::read(&mut reader);
+    println!("{:?}", header);
+
+    let header = header.unwrap();
+
+    let compression = String::from_utf8_lossy(&header.compression);
+    println!("Compression: {:?}", compression);
 
     PsarcFile {
         filename: String::from(path.to_string_lossy())
@@ -69,12 +71,13 @@ fn parse_psarc(path: &std::path::PathBuf) -> PsarcFile {
 
 fn main() -> std::io::Result<()> {
     // let args = Cli::parse(); // Implement later
-    let path: std::path::PathBuf = [
-        "/Users/livingston/dev/playground/realtime-audio-rs",
+
+    let mut path = std::env::current_dir().unwrap();
+    [
         "afx-song-file-toolkit",
         "bin/psarc/dlc/",
         "karmapolice_m.psarc"
-    ].iter().collect();
+    ].map(|x| path.push(x));
 
     let _ = parse_psarc(&path);
 
