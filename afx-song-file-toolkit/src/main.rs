2c5b64440ccbb7b9fc78d0fbd608c37ecb3579b3
Daniel Livingston <daniel.livingston@live.com>
2022-11-18 22:14:17 -0500

    Debugging support

--- afx-song-file-toolkit/src/main.rs
+++ afx-song-file-toolkit/src/main.rs
@@ -9,46 +9,6 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-#[derive(BinRead, Debug)]
-#[allow(dead_code)]
-struct Version {
-    major: u16,
-    minor: u16
-}
-
-#[derive(BinRead, Debug)]
-#[allow(dead_code)]
-struct TocTable {
-    md5_hash: u128,
-    block_offset: u32,
-    #[br(count = 5)]
-    uncompressed_size: Vec<u8>,
-    #[br(count = 5)]
-    file_offset: Vec<u8>
-}
-
-// https://www.psdevwiki.com/ps3/PlayStation_archive_(PSARC)
-// https://github.com/GaticusHax/libPSARC/blob/development/libPSARC-Static/Source/PSARC/Header.cs
-// https://docs.rs/binrw/latest/binrw/
-// https://jam1.re/blog/binread-a-declarative-rust-binary-parsing-library
-            #[derive(BinRead, Debug)]
-#[br(big, magic = b"PSAR")]
-#[allow(dead_code)]
-struct PsarcHeader {
-    version: Version,
-
-    #[br(count = 4, map = |bytes: Vec<u8>| String::from_utf8_lossy(&bytes).into_owned())]
-    compression_type: String,
-
-    toc_length: u32,
-    toc_entry_size: u32,
-    toc_entries: u32,
-    block_size: u32,
-    archive_flags: u32,
-
-    toc_table: TocTable
-}
-
 fn test(bytes: &Vec<u8>, little_endian: bool) -> u64 {
     match little_endian {
         true => {
@@ -68,25 +28,54 @@ fn test(bytes: &Vec<u8>, little_endian: bool) -> u64 {
     }
 }
 
+#[derive(BinRead, Debug)]
+#[allow(dead_code)]
+struct Version {
+    major: u16,
+    minor: u16
+}
+
+#[derive(BinRead, Debug)]
+#[allow(dead_code)]
+struct TocTable {
+    md5_hash: u128,
+    block_offset: u32,
+    #[br(count = 5, map = |bytes: Vec<u8>| test(&bytes, false))]
+    uncompressed_size: u64,
+    #[br(count = 5, map = |bytes: Vec<u8>| test(&bytes, false))]
+    file_offset: u64
+}
+
+#[derive(BinRead, Debug)]
+#[br(big, magic = b"PSAR")]
+#[allow(dead_code)]
+struct PsarcHeader {
+    version: Version,
+
+    #[br(count = 4, map = |bytes: Vec<u8>| String::from_utf8_lossy(&bytes).into_owned())]
+    compression_type: String,
+
+    #[br(map = |x: u32| x - 32)]
+    toc_length: u32,
+    toc_entry_size: u32,
+    toc_entries: u32,
+    block_size: u32,
+    archive_flags: u32,
+
+    toc_table: TocTable
+}
+
 fn test_results(header: &PsarcHeader) {
-    /*
-    (b'PSAR', 65540, b'zlib', 856, 30, 21, 65536, 4)
+    println!("Version: {}.{} == 65540", header.version.major, header.version.minor);
+    println!("Compression Type: {} == 'zlib'", header.compression_type);
+    println!("TOC Length: {} == 824 (856)", header.toc_length);
+    println!("TOC Entry Size: {} == 30", header.toc_entry_size);
+    println!("TOC Entries: {} == 21", header.toc_entries);
+    println!("Block Size: {} == 65536", header.block_size);
+    println!("Archive Flags: {} == 4", header.archive_flags);
 
-    MAGIC       = "PSAR" = header[0]
-    VERSION     = 65540  = header[1]
-    COMPRESSION = "zlib" = header[2]
-    toc_size    = 824    = header[3] - 32
-    BLOCK_SIZE  = 65536  = header[4]
-    n_entries   = 21     = header[5]
-    */
-
-    println!("Version: {}.{}", header.version.major, header.version.minor);
-    println!("Compression Type: {}", header.compression_type);
-    println!("TOC Length: {}", header.toc_length);
-    println!("TOC Entry Size: {}", header.toc_entry_size);
-    println!("TOC Entries: {}", header.toc_entries);
-    println!("Block Size: {}", header.block_size);
-    println!("Archive Flags: {}", header.archive_flags);
+    // toc table: should be an iterable
+    // all data below here is corrupted and must be uncompressed
     println!("TOC Table MD5 Hash: {}", header.toc_table.md5_hash);
     println!("TOC Table Block Offset: {}", header.toc_table.block_offset);
     println!("TOC Table Uncompressed Size: {:?}", header.toc_table.uncompressed_size);
@@ -100,14 +89,9 @@ fn parse_psarc(path: &std::path::PathBuf) -> PsarcHeader {
     let f = File::open(&path).unwrap();
     let mut reader = BufReader::new(f);
 
-    let header = PsarcHeader::read(&mut reader);
+    let header = PsarcHeader::read(&mut reader).unwrap();
     println!("{:?}", header);
-
-    let header = header.unwrap();
-    println!("block offset = {:?}", header.toc_table.block_offset);
-    println!("uncompressed size = {:?}", test(&header.toc_table.uncompressed_size, true));
-    println!("file offset = {:?}", test(&header.toc_table.file_offset, true));
-
+    test_results(&header);
     return header;
 }
 
