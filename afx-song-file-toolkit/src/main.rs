23e659806b6a6c9f7a2b3b1724064f982d9d6e49
Daniel Livingston <daniel.livingston@amd.com>
2022-11-21 17:37:51 -0500

    Basic AES

--- afx-song-file-toolkit/src/main.rs
+++ afx-song-file-toolkit/src/main.rs
@@ -4,11 +4,9 @@ use binrw::BinRead;
 use std::fs::File;
 use std::io::BufReader;
 
-use aes::cipher::{AsyncStreamCipher, KeyIvInit};
-use hex_literal::hex;
-
+// use aes::cipher::{AsyncStreamCipher, KeyIvInit};
 // https://github.com/RustCrypto/block-modes/blob/master/cfb-mode/Cargo.toml
-type Aes128CfbDec = cfb_mode::Decryptor<aes::Aes128>;
+// type Aes128CfbDec = cfb_mode::Decryptor<aes::Aes128>;
 
 #[derive(Parser)]
 struct Cli {
@@ -74,7 +72,7 @@ struct PsarcHeader {
 fn aes_cfb() {
     let key = 0x12;
     let iv  = 0x12;
-    Aes128CfbDec::new(&key.into(), &iv.into()).decrypt(&mut buf)
+    // Aes128CfbDec::new(&key.into(), &iv.into()).decrypt(&mut buf)
 }
 
 fn test_results(header: &PsarcHeader) {
@@ -94,6 +92,42 @@ fn test_results(header: &PsarcHeader) {
     println!("TOC Table File Offset: {:?}", header.toc_table.file_offset);
 }
 
+fn pad_zeroes<const A: usize, const B: usize>(arr: [u8; A]) -> [u8; B] {
+    assert!(B >= A); //just for a nicer error message, adding #[track_caller] to the function may also be desirable
+    let mut b = [0; B];
+    b[..A].copy_from_slice(&arr);
+    b
+}
+
+fn test_aes(buf: &mut Vec<u8>) {
+    use hex_literal::hex;
+    use aes::Aes128;
+    use aes::cipher::KeyIvInit;
+    use aes::cipher::AsyncStreamCipher;
+    use cfb_mode::Decryptor;
+
+    type Aes128CfbDec = Decryptor<Aes128>;
+
+    let key = hex!("C53DB23870A1A2F71CAE64061FDD0E1157309DC85204D4C5BFDF25090DF2572C");
+    let iv = hex!("E915AA018FEF71FC508132E4BB4CEB42");
+
+    // let key = [0x42; 16];
+    // let iv = [0x24; 16];
+
+    Aes128CfbDec::new(&key.into(), &iv.into()).decrypt(&mut buf);
+    // cipher_toc = AES.new(
+    //   codecs.decode(ARC_KEY,'hex'),  # secret key (16 bytes)
+    //   mode=AES.MODE_CFB,             # MODE_{EAX,CBC,CFB,OFB,OPENPGP}
+    //   IV=codecs.decode(ARC_IV,'hex'),# initialization vector (16 bytes)
+    //   segment_size=128)              # The number of bits the plaintext and 
+    //                                  # ciphertext are segmented in. 
+    //                                  # It must be a multiple of 8.
+    //
+    // let data = reader.read_bytes(toc_size);
+    // let data = data.pad();
+    // let decryption = cipher_toc.decrypt(&data);
+}
+
 fn parse_psarc(path: &std::path::PathBuf) -> PsarcHeader {
     let filename = String::from(path.to_string_lossy());
     println!("Filename: {:?}", filename);
@@ -104,6 +138,7 @@ fn parse_psarc(path: &std::path::PathBuf) -> PsarcHeader {
     let header = PsarcHeader::read(&mut reader).unwrap();
     println!("{:?}", header);
     test_results(&header);
+    //test_aes(&mut reader);
     return header;
 }
 
