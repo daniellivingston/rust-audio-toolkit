e6559b4e8a6d24e85aaee549ae4a5bea62704c98
Daniel Livingston <daniel.livingston@live.com>
2023-04-10 20:45:42 -0400

    Updated the script to support note names and added basic aubio

--- examples/fft.rs
+++ examples/fft.rs
@@ -6,38 +6,35 @@
 
 use hound::{WavReader, Error};
 use aubio::Notes;
+use aubio::{Pitch, PitchMode};
 
 fn process(samples: Vec<f32>, spec: &hound::WavSpec) {
-    let buff_size = 4096;
-    let hop_size = 1024;
+    let buff_size = 2048;
+    let hop_size = 256;
 
-    let onset_minioi = 0.0;
-    let silence_threshold = 0.0;
-    let release_drop = 0.0;
+    let mut pitch = Pitch::new(PitchMode::Yinfft, buff_size, hop_size, spec.sample_rate).unwrap();
+    let mut input = vec![0.0; hop_size];
+    let mut output = vec![0.0; 1];
 
-    println!("PROCESSING: {} samples", samples.len());
-
-    let notes = Notes::new(buff_size, hop_size, spec.sample_rate).unwrap()
-        //.with_minioi_ms(onset_minioi)
-        //.with_silence(silence_threshold)
-        //.with_release_drop(release_drop)
-        .do_result(samples);
-
-    println!("Finished");
-    println!("{:?}", notes);
+    for (i, sample) in samples.chunks_exact(hop_size).enumerate() {
+        input.copy_from_slice(sample);
+        pitch.do_(input.as_slice(), output.as_mut_slice()).unwrap();
+        println!("{}: {:?}", i, output);
+    }
 }
 
 fn read_wav_file(path: &str) -> Result<(), Error> {
     let mut reader = WavReader::open(path)?;
     let spec = reader.spec();
     let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
-    let samples: Vec<f32> = samples.iter().map(|&s| s as f32 / 32768.0).collect();
 
-    process(samples, &spec);
+    let s2: Vec<f32> = samples.iter().map(|s| *s as f32).collect();
+
+    process(s2, &spec);
 
     Ok(())
 }
 
 fn main() {
-    read_wav_file("examples/data/c3-major-scale-piano.wav").unwrap();
+    read_wav_file("examples/data/C_Major.wav").unwrap();
 }
--- examples/plot_data.py
+++ examples/plot_data.py
@@ -1,11 +1,30 @@
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
 import subprocess
 from pathlib import Path
+import mido
+import random
 
 SCRIPT_DIR = Path(__file__).parent
 
-AUDIO_FILE = f"{SCRIPT_DIR}/data/c3-major-scale-piano.wav"
+C_Major_Scale = {
+    "file": f"{SCRIPT_DIR}/data/C_major.wav",
+    "notes": ["C", "D", "E", "F", "G", "A", "B", "C", "B", "A", "G", "F", "E", "D", "C"],
+}
+
+AUDIO_FILE = C_Major_Scale["file"]
+
+def get_notes():
+    p = subprocess.run([
+           "aubionotes",
+           AUDIO_FILE
+        ],
+        capture_output=True,
+        check=True
+    )
+    output = p.stdout.decode("utf-8").splitlines()
+    return np.array([list(map(float, line.split())) for line in output[1:-1]])
 
 def get_beats():
     p = subprocess.run([
@@ -75,29 +94,77 @@ with open(f"{SCRIPT_DIR}/data/notes.txt", "r") as f:
 # ----------------------------------------------------- #
 
 freqs = get_frequencies(PITCH_METHODS[0])
+onsets = get_onsets()
 
 print("time, frequency")
 print(freqs)
+
+print("onsets", onsets)
+
+# ----------------------------------------------------- #
+
+time = freqs[:,0]
+idx = np.array(list(map(lambda x: np.argmin(np.abs(time - x)), onsets)))
+
+x = np.zeros((len(onsets) - 1,))
+captured_notes = []
+
+for i in range(len(idx) - 1):
+    x[i] = np.mean(freqs[idx[i]:idx[i+1], 1])
+    captured_notes.append(note_names[np.argmin(np.abs(note_freqs - x[i]))])
+
+print(x)
+print(captured_notes)
 
 # ----------------------------------------------------- #
 
 min_freq = np.min(freqs[:, 1])
 max_freq = np.max(freqs[:, 1])
 
-fig, axes = plt.subplots(nrows=1, ncols=1)
+fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True)
 
 fig.suptitle(Path(AUDIO_FILE).stem)
+
+# --- FREQUENCY ------------------------------------------------------ #
 
 # for beat in get_beats():
 #     axes.axvline(beat, color="grey", linestyle="--")
 
-for onset in get_onsets():
-    axes.axvline(onset, color="green", linestyle="--")
+for onset in onsets:
+    axes[0].axvline(onset, color="green", linestyle="--")
 
 for (name, freq) in zip(note_names, note_freqs):
-    axes.annotate(name, xy=(0.0, freq))
-    axes.axhline(freq, color="red", linestyle="--")
+    axes[0].annotate(name, xy=(0.0, freq))
+    axes[0].axhline(freq, color="red", linestyle="--")
 
-axes.scatter(freqs[:, 0], freqs[:, 1])
-axes.set_ybound(min_freq, max_freq)
+axes[0].scatter(freqs[:, 0], freqs[:, 1])
+axes[0].set_ybound(min_freq, max_freq)
+
+# --- NOTES ---------------------------------------------------------- #
+
+def get_note_name(midi_note):
+    # Calculate pitch value (number of semitones away from A4)
+    pitch = midi_note - 69
+
+    # Calculate octave and note name
+    octave = pitch // 12
+    note = pitch % 12
+    note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    note_name = note_names[note]
+
+    # Return note name with octave number
+    return f"{note_name}{octave}"
+
+notes = get_notes()
+
+axes[1].scatter(notes[:, 1], notes[:, 0])
+
+for note in notes:
+    name = get_note_name(int(note[0]))
+    axes[1].annotate(name, xy=(note[1], note[0]))
+
+
 plt.show()
+
+
+# ----------------------------------------------------- #
