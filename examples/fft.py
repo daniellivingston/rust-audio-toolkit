de26fd4e0cac7a81bff612d228f0313ca6ae0fc4
Daniel Livingston <daniel.livingston@amd.com>
2023-06-13 13:15:58 -0400

    Add freq

--- examples/fft.py
+++ examples/fft.py
@@ -24,6 +24,9 @@ import math
 def freq_to_note(freq):
     notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
+    if freq == 0.:
+        return "", 0
+
     note_number = 12 * math.log2(freq / 440) + 49
     note_number = round(note_number)
 
@@ -76,8 +79,15 @@ def gen_notes(filename: str, samplerate: int = None):
     win_s = 512 // downsample # fft size
     hop_s = 256 // downsample # hop size
 
-    s = aubio.source(filename, samplerate, hop_s)
-    print(f"{s}")
+    #s = aubio.source(filename, samplerate, hop_s)
+    s = aubio.source(filename, hop_size=hop_s)
+    print(f"""{filename}
+------------------------
+URI: {s.uri}
+Sample Rate: {s.samplerate}
+Duration: {s.duration}
+Channels: {s.channels}
+    """)
     samplerate = s.samplerate
 
     tolerance = 0.8
@@ -146,83 +156,47 @@ def gen_notes(filename: str, samplerate: int = None):
     # play_midi(filename)
     return _freqs
 
-def gen_freqs(filename: str, samplerate: int = None):
+def gen_freqs(filename: str, min_confidence = 0.3):
     downsample = 1
-
-    if samplerate is None:
-        samplerate = 44100 // downsample
 
     win_s = 512 // downsample # fft size
     hop_s = 256 // downsample # hop size
 
-    s = aubio.source(filename, samplerate, hop_s)
-    print(f"{s}")
-    samplerate = s.samplerate
+    with aubio.source(filename, hop_size=hop_s) as source:
+        print(f"""{filename}
+    ------------------------
+    URI: {source.uri}
+    Sample Rate: {source.samplerate}
+    Duration: {source.duration}
+    Channels: {source.channels}
+        """)
+        samplerate = source.samplerate
 
-    tolerance = 0.8
+        # --- Begin Analysis -------------------------------------- #
+        pitch_o = aubio.pitch("default",
+                              buf_size=win_s,
+                              hop_size=hop_s,
+                              samplerate=samplerate)
 
-    notes_o = aubio.notes("default", win_s, hop_s, samplerate)
+        # --- Read samples from file ------------------------------------- #
+        _freqs = []
+        i = 0
+        total_frames = 0
+        while True:
+            samples, read = source()
+            total_frames += read
+            if read < hop_s:
+                break
 
-    # --- Initialize MIDI Track -------------------------------------- #
-    midi = MidiFile()
-    track = MidiTrack()
-    midi.tracks.append(track)
+            pitch = pitch_o(samples)
+            confidence = pitch_o.get_confidence()
 
-    ticks_per_beat = midi.ticks_per_beat # default: 480
-    bpm = 120 # default midi tempo
+            if (confidence > min_confidence):
+                _freqs.append(pitch[0])
+                time = i * hop_s
 
-    tempo = bpm2tempo(bpm)
-    track.append(MetaMessage('set_tempo', tempo=tempo))
-    track.append(MetaMessage('time_signature', numerator=4, denominator=4))
+            i += 1
 
-    def frames2tick(frames, samplerate=samplerate):
-        sec = frames / float(samplerate)
-        return int(second2tick(sec, ticks_per_beat, tempo))
-
-    # --- Read samples from file ------------------------------------- #
-    _freqs = []
-    last_time = 0
-    total_frames = 0 # total number of frames read
-    while True:
-        samples, read = s()
-        new_note = notes_o(samples)
-
-        if (new_note[0] != 0): # valid note found
-            delta = frames2tick(total_frames) - last_time
-
-            # Add 'note_off' to track, if applicable
-            if new_note[2] > 0:
-                msg = Message(
-                    'note_off',
-                    note=int(new_note[2]),
-                    velocity=127,
-                    time=delta
-                )
-                track.append(msg)
-
-            # Add 'note_on' to track
-            msg = Message(
-                'note_on',
-                note=int(new_note[0]),
-                velocity=int(new_note[1]),
-                time=delta
-            )
-            track.append(msg)
-
-            print("%.2f : %s" % (delta, midi_note_to_str(new_note[0])))
-            _freqs.append(miditofreq(new_note[0]))
-
-            last_time = frames2tick(total_frames)
-
-        total_frames += read
-        if read < hop_s:
-            break
-
-    print("Finished reading file:", filename)
-    # filename = filename.replace(".wav", ".mid")
-    # midi.save(filename)
-    # print("Saved MIDI file:", filename)
-    # play_midi(filename)
     return _freqs
 
 freq1 = gen_freqs(WAV_C_MAJOR)
